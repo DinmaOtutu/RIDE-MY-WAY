76bb454e60429054bd3cadbed599aa2f72395e70
DinmaOtutu <otutudinma1995@gmail.com>
2018-06-23 01:26:34 +0100

    data for testing api endpoints

---
+++ test/utils/signin.setup.js
@@ -0,0 +1,32 @@
+import chai from 'chai';
+
+import chaiHttp from 'chai-http';
+
+import { passengers, drivers } from '../../server/data';
+
+import app from '../../server/app';
+
+chai.use(chaiHttp);
+
+const api = `/api/${process.env.VERSION}`;
+
+
+// logs in and returns a token
+
+export async function passengerToken(passengerId) {
+  const { email, password } = passengers[passengerId];
+
+  const res = await chai.request(app)
+    .post(`${api}/signin`)
+    .send({ email, password });
+  return res.body.token;
+}
+
+export async function driverToken(driverId) {
+  const { email, password } = drivers[driverId];
+
+  const res = await chai.request(app)
+    .post(`${api}/signin`)
+    .send({ email, password });
+  return res.body.token;
+}
